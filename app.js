733d74d9080b8bea46bc65482a82f3ad30f56e00
Mohamed Kamal <mohamed77zaki@gmail.com>
2019-09-16 23:49:22 +0200

    Section 4 Step 2

--- app.js
+++ app.js
@@ -2,7 +2,5 @@ const validator = require('validator');
 const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
-const msg = getNotes();
-console.log(msg);
-
-console.log(chalk.bold.blue.inverse('Success!'));
+console.log(process.argv);
+console.log(process.argv[2]);
