20369c0a5ea0d724e28406a71269900e930585b1
Mohamed Kamal <mohamed77zaki@gmail.com>
2019-09-19 02:47:50 +0200

    Section 4 Step 6

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const validator = require('validator');
 const chalk = require('chalk');
 const yargs = require('yargs');
-const getNotes = require('./notes.js');
+const notes = require('./notes.js');
 
 // Customize yargs version
 yargs.version('1.1.0');
@@ -23,8 +23,7 @@ yargs.command({
   	}
   },
   handler: (argv) => {
-  	console.log('title: ' + argv.title);
-  	console.log('body: ' + argv.body);
+  	notes.addNote(argv.title, argv.body);
   }
 });
 
--- notes.js
+++ notes.js
@@ -1,3 +1,39 @@
+const fs = require('fs');
+
+const loadNotes = () => {
+  try {
+  	const datasBuffer = fs.readFileSync('notes.json');
+    const dataJSON = datasBuffer.toString();
+    const notes = JSON.parse(dataJSON);
+    return notes;
+  } catch (error) {
+  	return [];
+  }
+};
+
+const saveNotes = (notes) => {
+  const notesJSON = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', notesJSON);
+};
+
 const getNotes = () => "Your notes...";
 
-module.exports = getNotes;
+const addNote = (title, body) => {
+  const notes = loadNotes();
+  const dublicatedNote = notes.filter(note => note.title == title);
+  if(dublicatedNote.length == 0) {
+  	notes.push({
+  		title: title,
+  		body: body
+  	});
+  	saveNotes(notes);
+  	console.log(loadNotes());
+  } else {
+  	console.log('A note with the same title already exist!');
+  }
+}
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote
+};
